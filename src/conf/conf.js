05c1508c9504e1eaa02746424e220546de91bff2
Prashant <Prashantsehgal03@gmail.com>
2024-04-18 20:19:01 +0530

    build Authentication part

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUUCKET_ID),
+}
+
+export default conf;
