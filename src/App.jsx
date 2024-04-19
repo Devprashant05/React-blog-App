3600f45ad23215b7069d005b2b5226289a46daa7
Prashant <Prashantsehgal03@gmail.com>
2024-04-19 13:48:22 +0530

    Build Service part and add redux functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import "./App.css";
-
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/reducers/AuthReducer";
+import { Header, Footer } from "./components/index";
 function App() {
-    console.log(import.meta.env.VITE_APPWRITE_URL);
-    return (
-        <>
-            <h1>A blog app with appwrite</h1>
-        </>
-    );
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        authService
+            .getCurrentUser()
+            .then((userData) => {
+                if (userData) {
+                    dispatch(login(userData));
+                } else {
+                    dispatch(logout());
+                }
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return !loading ? (
+        <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+            <div className="w-full block">
+                <Header />
+                <main> Todo: {/* Outlet */}</main>
+                <Footer />
+            </div>
+        </div>
+    ) : null;
 }
 
 export default App;
